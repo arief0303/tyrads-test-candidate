250ae9ac90236238e568776e91c500420983a637
Arief R Syauqie <arief0303@gmail.com>
2024-02-14 21:07:50 +0700

    header

---
+++ src/components/Header.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="navbar bg-base-100 pl-20">
+      <div className="navbar-start">
+        <article className="prose prose-sm">
+          <h1 className="m-0">Hello, Jhon .D</h1>
+          <p className="m-0">View and control your finances here!</p>
+        </article>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a>Item 3</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end pr-64">
+        <button className="btn btn-ghost btn-circle">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
