1cc9ab53714c13d6f1452e7f9b303dfa9aba394b
Arief R Syauqie <arief0303@gmail.com>
2024-02-14 21:08:18 +0700

    header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
 
 export default function index() {
   return (
     <>
       <Sidebar />
+      <Header />
     </>
   );
 }
