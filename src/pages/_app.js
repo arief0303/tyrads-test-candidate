406c4e0e03d3ae0f4553a18b9365c3de8fa8a531
Arief R Syauqie <arief0303@gmail.com>
2024-02-15 03:23:36 +0700

    framer motion page transition

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,23 @@
 import "@/styles/globals.css";
 import Sidebar from "@/components/Sidebar";
+import { AnimatePresence, motion } from 'framer-motion';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, router }) {
   return (
     <div>
       <Sidebar />
-      <Component {...pageProps} />
+      <AnimatePresence mode='wait'>
+        <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" variants={{
+          pageInitial: {
+            opacity: 0
+          },
+          pageAnimate: {
+            opacity: 1
+          },
+        }}>
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
