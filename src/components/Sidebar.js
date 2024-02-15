babe9a39db8931fb657dd957441b7fee1fec34d1
Arief R Syauqie <arief0303@gmail.com>
2024-02-15 14:30:27 +0700

    added transactions link

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -139,6 +139,28 @@ const Sidebar = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3"
+                />
+              </svg>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={"/transactions"}
+              className="p-2 tooltip tooltip-right btn-ghost btn-circle flex items-center justify-center"
+              data-tip="Transactions"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
                 strokeWidth="1.5"
                 stroke="currentColor"
                 className="w-5 h-5"
@@ -180,7 +202,11 @@ const Sidebar = () => {
           </li>
         </ul>
         <br />
-        <div className={`profile-section ${router.pathname === '/login' ? 'hidden' : ''}`}>
+        <div
+          className={`profile-section ${
+            router.pathname === "/login" ? "hidden" : ""
+          }`}
+        >
           <ul className="menu-vertical bg-gray-200 m-4 rounded-full flex items-center justify-center">
             <div className="flex">
               <div className="dropdown dropdown-right">
