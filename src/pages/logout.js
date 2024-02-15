f06a63e1163e92795a6b4ac330fd72c1d3f1fbe1
Arief R Syauqie <arief0303@gmail.com>
2024-02-15 14:17:47 +0700

    logout cookies implementation

--- src/pages/logout.js
+++ src/pages/logout.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from 'next/router';
+import Cookies from 'js-cookie';
 
 function checkUserAuthentication(req) {
   // Check the request headers, cookies, etc. to see if the user is authenticated.
@@ -23,10 +25,26 @@ export async function getServerSideProps(context) {
 }
 
 const logout = () => {
+  const router = useRouter();
+  const [seconds, setSeconds] = useState(5);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds(seconds => seconds > 0 ? seconds - 1 : 0);
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(intervalId);
+      Cookies.remove('authenticated');
+      router.push('/login');
+    }, 5000);
+  }, []);
+
   return (
     <div className="bye" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <article className="prose lg:prose-xl">
         <h1 className="pl-[6rem]">Bye bye! 😊</h1>
+        <p className="pl-[6rem]">You will logout in {seconds} seconds</p>
       </article>
     </div>
   );
